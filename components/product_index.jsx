434d8356edae0bbe44d7513c096ef136aa5c5616
michaeljstevens <michaeljstevens@zoho.com>
2016-11-27 00:47:32 -0800

    ajax call working

---
+++ components/product_index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductIndex extends Component {
+  render() {
+    return(
+      <div>
+        ProductIndex
+      </div>
+    );
+  }
+}
+
+export default ProductIndex;
--- components/root.jsx
+++ components/root.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import ProductIndex from './product_index.jsx';
+import $ from 'jquery';
 
 class Root extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: null
+    };
+  }
+
+  componentDidMount() {
+    const error = message => console.log(message);
+    const success = response => {
+      debugger
+    };
+    $.ajax({
+      method: 'GET',
+      url: 'https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js',
+      dataType: 'JSON',
+      success,
+      error
+    });
+  }
+
   render() {
     return(
       <div>
-        react root
+        <ProductIndex />
       </div>
     );
   }
